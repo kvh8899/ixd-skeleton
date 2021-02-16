84bab221e063ab13ccca882a3ae1468ecb4caece
Kyle <kvhuang@ucsd.edu>
2021-02-15 22:26:22 -0800

    fixed glitch

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,3 @@
-
-/*
- * GET home page.
- */
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
+exports.view = function(req, res){
+  res.render('index');
+};
